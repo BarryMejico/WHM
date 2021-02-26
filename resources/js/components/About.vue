23aba0102241674914c3a63510933a8965bc4e61
abi-collab <72607892+abi-collab@users.noreply.github.com>
2021-02-26 13:42:46 +0800

    About Us/Footer logo & content
    initial push

--- resources/js/components/About.vue
+++ resources/js/components/About.vue
@@ -5,11 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
 
-                <div align="center">
-                  
-                      
-                         <img src="../media/barkspider2 copy.png" alt="logo" style="width:50%;">
-                  
+      
+                <div align="center">  
+                         <img src="../media/bs3.png" alt="logo" style="width:60%;">
                 </div>
                 <br>
                 <br>
@@ -17,16 +15,59 @@
 
                 <div>
                    
-                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<j>A group of freelance web developers innovating web services to give Filipino entrepreneurs a tool for their businesses.</j>
-                   <br><br>
-                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<j>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                   labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                   nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-                   dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</j>
-                    <br><br>
-                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<j>Risus sed vulputate odio ut. Auctor augue mauris augue neque gravida in.
-                    Quam vulputate dignissim suspendisse in est ante in nibh. Interdum varius sit amet mattis vulputate. 
-                    Amet nisl suscipit adipiscing bibendum. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium. </j>
+                   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<j class="lead text-muted">A group of freelance web developers innovating web services to give Filipino entrepreneurs a modern tool for their businesses.</j>
+                  <hr><br>
+                   <h4 class="text-muted"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We Offer:</h4>
+                   <br>
+                   <br>
+                   <br>
+
+                   <div class="row">
+        
+                       <div class="col-md-6">
+                           <img class="displayed" src="../media/SaaS.png" alt="logo">
+                       </div>
+                       <div class="col-md-6 a">
+                           <h5>Software as a Service</h5>
+                           <small class="text-muted">Software delivery and licensing in which software is accessed online via a subscription</small>
+                       </div>
+                   </div>
+               <br>
+
+                   <div class="row">
+                       <div class="col-md-6">
+                           <img class="displayed" src="../media/online platform copy.png" alt="logo">
+                       </div>
+                       <div class="col-md-6 a">
+                           <h5>Online Platform for Business and Marketing</h5>
+                           <small class="text-muted">Leveraging web-based channels to spread a message about a company's brand, products, or services to its potential customers</small>
+                       </div>
+                   </div>
+
+                <br>
+
+                   <div class="row">
+                       <div class="col-md-6">
+                           <img class="displayed" src="../media/business solutions copy.png" alt="logo">
+                       </div>
+                       <div class="col-md-6 a">
+                           <h5>Modern Approach of Business Solutions</h5>
+                           <small class="text-muted">Modern workforce management solution helps a business organization to be both more efficient </small>
+                       </div>
+                   </div>
+
+               <br>
+
+                   <div class="row">
+                       <div class="col-md-6">
+                           <img class="displayed" src="../media/business subscription plan copy.png" alt="logo">
+                       </div>
+                       <div class="col-md-6 a">
+                           <h5>Taylored Fit Description Plan for the Needs of Different Businesses</h5>
+                           <small class="text-muted">A roadmap that gives you a step by step guide to creating ongoing value for your customers</small>
+                       </div>
+                   </div>
+                   
                 </div>
            
 
@@ -37,3 +78,19 @@
 </template>
 <script>
 </script>
+<style scoped>
+.displayed{
+    display: block;
+   
+    margin:auto auto auto 60%;
+    width:30%;
+}
+.a{
+    margin:auto;
+    align-items: center
+}
+h5{
+    color:#c69c6d;
+}
+
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -162,12 +162,15 @@
         <div class="row">
         	<div class="col-md-4 footer-brand animated fadeInLeft">
             <!----------------- di binabasa ------------------------>
-            <img src="../media/barkspider2 copy.png" alt="logo" style="width:280px;">
+            <img src="../media/bs3.png" alt="logo" style="width:50%;">
             <!----------------- pero eto binabasa ---------------------
             <img src="../media/logo.png" alt="logo" style="width:40px;">--->
                 <br>
          
-                    <p id="bbb">The website footer is not to be neglected. According to research and different studies, it is a favourable place to capitalize on to increase conversions. Depending on the website type and the objectives of each page, the footer might include different elements of use for the visitors. The only thing you have to do is choose the RIGHT elements to put in the website footer, and organize them according to a clean structure, easy to browse and take benefit from.</p>
+                    <p id="bbb">Empowering enterprises to use technology innovations for growth and success,
+                         offering products for businesses and operations, and deliver effective IT solutions
+                        with quality service to enhance the competitive advantages.
+                        </p>
                 
                     <p>© 2014 BS3 UI Kit, All rights reserved</p>
             </div>
